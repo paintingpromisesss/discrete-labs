6fcce284fad136b4650567ce8a38fdabfb0664df
paintingpromisesss <paintingpromisesss@gmail.com>
2025-05-10 17:58:31 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,63 +1,39 @@
-#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <utility>
-#include <cstdio>
-#include <ctime>
-const int MAX = 65536;
+#include <string>
+#include <iostream>
 
-void countSort(std::vector<std::pair<unsigned short, unsigned long long>> &data) {
-    if (data.empty()) return;
-
-    unsigned short minKey = std::min_element(data.begin(), data.end(),
-        [](const auto &a, const auto &b){ return a.first < b.first; })->first;
-    unsigned short maxKey = std::max_element(data.begin(), data.end(),
-        [](const auto &a, const auto &b){ return a.first < b.first; })->first;
-
-    std::vector<unsigned int> counts(maxKey - minKey + 1, 0);
-
-    for (const auto &elem : data) {
-        counts[elem.first - minKey]++;
+std::vector<int> z_function(const std::string& s) {
+    int n = s.size();
+    std::vector<int> z(n, 0);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; ++i) {
+        if (i <= r)
+            z[i] = std::min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
     }
-
-    for (size_t i = 1; i < counts.size(); i++) {
-        counts[i] += counts[i - 1];
-    }
-
-    std::vector<std::pair<unsigned short, unsigned long long>> sortedData(data.size());
-    for (int i = data.size() - 1; i >= 0; i--) {
-        sortedData[counts[data[i].first - minKey] - 1] = data[i];
-        counts[data[i].first - minKey]--;
-    }
-
-    data = std::move(sortedData);
+    return z;
 }
 
-int main(int argc, char *argv[]) {
-    std::vector<std::pair<unsigned short, unsigned long long>> data;
-    FILE *inFile = fopen(argv[1], "r");
-    unsigned short first;
-    unsigned long long second;
-    while (fscanf(inFile, "%hu\t%llu", &first, &second) == 2) {
-        data.push_back(std::make_pair(first, second));
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string text, pattern;
+    std::cin >> text >> pattern;
+
+    std::string concat = pattern + '$' + text;
+    std::vector<int> z = z_function(concat);
+    int pat_len = pattern.size();
+
+    for (int i = pat_len + 1; i < (int)z.size(); ++i) {
+        if (z[i] == pat_len) {
+            std::cout << (i - pat_len - 1) << "\n";
+        }
     }
-    fclose(inFile);
-    
-    clock_t start = clock();
-    
-    countSort(data);
-
-    clock_t end = clock();
-    double elapsed = double(end - start) / CLOCKS_PER_SEC;
-
-    std::cout << "Elapsed time: " << elapsed << " seconds" << std::endl;
-
-    FILE *outFile = fopen(argv[2], "w");
-    for (size_t i = 0; i < data.size(); i++) {
-        fprintf(outFile, "%hu\t%llu\n", data[i].first, data[i].second);
-    }
-    fclose(outFile);
-
-    
     return 0;
 }
