2c73a4d8454a1db0056741f31b36cc7be05f781c
paintingpromisesss <paintingpromisesss@gmail.com>
2025-10-31 17:13:12 +0300

    done ez

--- main.cpp
+++ main.cpp
@@ -1,40 +1,59 @@
-#include <vector>
-#include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
 
-unsigned long long dp(int n, const std::string &s)
+void dfs(int v, std::vector<std::vector<int>> &graph, std::vector<int> &color, std::vector<int> &result, bool &is_cycle)
 {
-    std::vector<std::vector<unsigned long long>> table(n, std::vector<unsigned long long>(n, 0));
-
-    for (int i = 0; i < n; ++i)
+    color[v] = 1;
+    for (int u : graph[v])
     {
-        table[i][i] = 1;
-    }
-
-    for (int length = 2; length <= n; ++length)
-    {
-        for (int i = 0; i <= n - length; ++i)
+        if (color[u] == 1)
         {
-            int j = i + length - 1;
-
-            if (s[i] != s[j])
-            {
-                table[i][j] = table[i + 1][j] + table[i][j - 1] - table[i + 1][j - 1];
-            }
-            else
-            {
-                table[i][j] = table[i + 1][j] + table[i][j - 1] + 1;
-            }
+            is_cycle = true;
+            return;
         }
+        if (color[u] == 0)
+            dfs(u, graph, color, result, is_cycle);
     }
-    return table[0][n - 1];
+    color[v] = 2;
+    result.push_back(v);
 }
 
 int main()
 {
-    std::string s;
-    std::cin >> s;
-    int n = s.length();
-    std::cout << dp(n, s) << std::endl;
-    return 0;
+    bool is_cycle = false;
+
+    int n, m;
+    std::cin >> n >> m;
+
+    std::vector<std::vector<int>> graph(n + 1);
+
+    std::vector<int> color(n + 1, 0), result;
+
+    int arg = INT32_MAX;
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        graph[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (color[i] == 0) dfs(i, graph, color, result, is_cycle);
+    }
+
+    if (is_cycle)
+    {
+        std::cout << -1 << std::endl;
+    }
+    else
+    {
+        for (int i = n - 1; i > 0; i--)
+        {
+            std::cout << result[i] << " " << result[i - 1] << std::endl;
+        }
+    }
 }
